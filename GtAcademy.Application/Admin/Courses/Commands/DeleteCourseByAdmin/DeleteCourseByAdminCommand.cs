b6b871f784e7a4c57d11b7e6db74c6eb1aac3a95
hossein <hossein.ranjbari06@gmail.com>
2026-04-10 10:29:26 -0700

    Delete Course by admin

---
+++ GtAcademy.Application/Admin/Courses/Commands/DeleteCourseByAdmin/DeleteCourseByAdminCommand.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GtAcademy.Application.Admin.Courses.Commands.DeleteCourseByAdmin
+{
+    public record DeleteCourseByAdminCommand(Guid CourseId) : IRequest<ErrorOr<bool>>;
+}
---
+++ GtAcademy.Application/Admin/Courses/Commands/DeleteCourseByAdmin/DeleteCourseByAdminCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GtAcademy.Application.Admin.Courses.Commands.DeleteCourseByAdmin
+{
+    public class DeleteCourseByAdminCommandHandler : IRequestHandler<DeleteCourseByAdminCommand, ErrorOr<bool>>
+    {
+        public Task<ErrorOr<bool>> Handle(DeleteCourseByAdminCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
